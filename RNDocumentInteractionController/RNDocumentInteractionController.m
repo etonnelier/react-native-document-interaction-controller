e71028e94c8a1bacdceafc4475d24d746bfd889e
etonnelier <eti.tonnelier@gmail.com>
2022-06-26 13:19:12 +0200

    using method from https://github.com/twksos/react-native-document-interaction-controller

--- RNDocumentInteractionController/RNDocumentInteractionController.m
+++ RNDocumentInteractionController/RNDocumentInteractionController.m
@@ -13,21 +13,30 @@
 
 RCT_EXPORT_MODULE();
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
 
-RCT_EXPORT_METHOD(open: (NSURL *)path)
+RCT_EXPORT_METHOD(preview:(NSString *)fileURL resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    UIDocumentInteractionController *interactionController = [UIDocumentInteractionController interactionControllerWithURL:path];
-    interactionController.delegate = self;
-    [interactionController presentPreviewAnimated:YES];
-}
+    NSURL *url = [NSURL fileURLWithPath:[fileURL stringByAddingPercentEncodingWithAllowedCharacters: [NSCharacterSet URLQueryAllowedCharacterSet]]];
 
-- (UIViewController *) documentInteractionControllerViewControllerForPreview: (UIDocumentInteractionController *) controller
-{
-    return [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+    UIDocumentInteractionController *viewController = [UIDocumentInteractionController interactionControllerWithURL:url];
+    
+    viewController.delegate = self;
+    
+    if (![viewController respondsToSelector:@selector(presentPreviewAnimated:)]) {
+        NSError *error = [[NSError alloc] init];
+        reject(@"cannot_preview", @"Can not preview", error);
+    } else {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            BOOL canOpen = [viewController presentPreviewAnimated:YES];
+            NSMutableArray *result = [[NSMutableArray alloc] init];
+            if(canOpen) resolve(result);
+            else {
+                NSError *error = [[NSError alloc] init];
+                reject(@"cannot_handle_file", @"Can not handle file", error);
+            }
+        });
+    }
 }
 
 
